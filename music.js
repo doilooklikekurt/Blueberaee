7569e35040c502d3457de26b0ec6e171450768fb
Whoisme? <94670139+doilooklikekurt@users.noreply.github.com>
2022-09-04 10:08:22 +0800

    Initial

---
+++ music.js
@@ -0,0 +1,43 @@
+function playPause() {
+
+    document.getElementById('song').paused
+        ? document.getElementById('song').play()
+        : document.getElementById('song').pause()
+
+    document.querySelector('.audio-wire-1').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-1').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-1').style.animationPlayState = "paused"
+
+    document.querySelector('.audio-wire-2').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-2').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-2').style.animationPlayState = "paused"
+
+    document.querySelector('.audio-wire-3').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-3').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-3').style.animationPlayState = "paused"
+
+    document.querySelector('.audio-wire-4').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-4').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-4').style.animationPlayState = "paused"
+
+    document.querySelector('.audio-wire-5').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-5').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-5').style.animationPlayState = "paused"
+
+    document.querySelector('.audio-wire-6').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-6').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-6').style.animationPlayState = "paused"
+
+    document.querySelector('.audio-wire-7').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-7').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-7').style.animationPlayState = "paused"
+
+    document.querySelector('.audio-wire-8').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-8').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-8').style.animationPlayState = "paused"
+
+    document.querySelector('.audio-wire-9').style.animationPlayState === "paused"
+        ? document.querySelector('.audio-wire-9').style.animationPlayState = "running"
+        : document.querySelector('.audio-wire-9').style.animationPlayState = "paused"
+}
+
